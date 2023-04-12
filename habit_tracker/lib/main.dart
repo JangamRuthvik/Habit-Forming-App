077b0adbf1fa18d999828486bda6f9c237670b02
unknown <jruthvik7171@gmail.com>
2023-04-12 22:49:51 +0530

    added main file

---
+++ habit_tracker/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: NavigationScreen(
+        currentIndex: 2,
+      ),
+    );
+  }
+}
+
+class NavigationScreen extends StatefulWidget {
+  NavigationScreen({required this.currentIndex});
+  int currentIndex;
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: widget.currentIndex,
+        // children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black87,
+        unselectedItemColor: Colors.greenAccent,
+        selectedItemColor: Colors.deepPurpleAccent,
+        currentIndex: widget.currentIndex,
+        onTap: (index) {
+          setState(() {
+            widget.currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today), label: "Progress"),
+          BottomNavigationBarItem(icon: Icon(Icons.timeline), label: "Habits"),
+          // BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+        ],
+        ),
+        );
+        }
+}
