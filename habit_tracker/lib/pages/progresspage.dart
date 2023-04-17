bc4a576dda826c10ffa5043b18beba2abccbea0d
sujith <makamsujith2004@gmail.com>
2023-04-17 20:52:42 +0530

    background pic added

--- habit_tracker/lib/pages/progresspage.dart
+++ habit_tracker/lib/pages/progresspage.dart
@@ -22,14 +22,14 @@ class _ProgressPageState extends State<ProgressPage> {
           child: ClipRRect(
             borderRadius: BorderRadius.circular(30),
             child: Stack(children: [
-              Image.asset('assets/progressPageBackground.png'),
+              Image.asset('assets/progress.jpg'),
               Padding(
                 padding: EdgeInsets.fromLTRB(20, 140, 0, 20),
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: Column(children: const [
                     Text(
-                      "Hey Hermano!",
+                      "Hey Buddy!",
                       style: TextStyle(
                           fontSize: 22,
                           fontWeight: FontWeight.bold,
